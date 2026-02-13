8e670b9f1d04f0ce2dc5574708cc3902572412ae
Hairum Qureshi <hqureshi@udel.edu>
2026-02-13 18:55:52 -0500

    imported Mongoose Doc model schema

--- apps/backend/src/document/document.module.ts
+++ apps/backend/src/document/document.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { Doc, DocSchema } from 'src/schemas/Document';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   providers: [DocumentService],
   controllers: [DocumentController],
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Doc.name,
+        schema: DocSchema,
+      },
+    ]),
+    AuthModule,
+  ],
 })
 export class DocumentModule {}
