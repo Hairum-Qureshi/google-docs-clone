8acdbd5fa4eb9dd3a8d180748c8c2bc0b723e3b0
Hairum Qureshi <hqureshi@udel.edu>
2026-02-13 11:42:56 -0500

    added 'AuthModule' to imports array

--- apps/backend/src/document/document.module.ts
+++ apps/backend/src/document/document.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [DocumentService],
-  controllers: [DocumentController]
+  controllers: [DocumentController],
+  imports: [AuthModule],
 })
 export class DocumentModule {}
