c7b515e91509fc417d1e21f5410c29febe4b6ee7
Hairum Qureshi <hqureshi@udel.edu>
2026-02-13 18:58:54 -0500

    created method for creating a document and saving it to the DB

--- apps/backend/src/document/document.service.ts
+++ apps/backend/src/document/document.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Doc, DocDocument } from 'src/schemas/Document';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  constructor(@InjectModel(Doc.name) private docModel: Model<DocDocument>) {}
+
+  async createDocument(userID: string) {
+    const newDoc = new this.docModel({
+      authorUID: userID,
+      title: 'Untitled Document',
+    });
+    await newDoc.save();
+    return newDoc._id;
+  }
+}
