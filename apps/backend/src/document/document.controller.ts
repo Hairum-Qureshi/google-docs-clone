54e40c959343cdeb758b3b26879f77f91d594bf2
Hairum Qureshi <hqureshi@udel.edu>
2026-02-13 18:58:21 -0500

    created POST endpoint for creating a new doc

--- apps/backend/src/document/document.controller.ts
+++ apps/backend/src/document/document.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { DocumentService } from './document.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import * as types from 'src/types';
 
-@Controller('document')
-export class DocumentController {}
+@Controller('api/document')
+export class DocumentController {
+  constructor(private documentService: DocumentService) {}
+
+  @Post('new')
+  @UseGuards(AuthGuard())
+  async createDocument(@CurrentUser() user: types.UserPayload) {
+    const documentId = await this.documentService.createDocument(user._id);
+    return { documentId };
+  }
+}
