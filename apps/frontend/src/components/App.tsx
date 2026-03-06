b2ba467bd7b23e10d9fd8a46bbd0a317e020beca
Hairum Qureshi <hqureshi@udel.edu>
2026-03-06 15:10:48 -0500

    added 'my-documents' route

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -3,13 +3,17 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import "../css/index.css";
+import Navbar from "./Navbar";
+import MyDocuments from "../pages/MyDocuments";
 
 export default function App() {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/my-documents" element={<MyDocuments />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
