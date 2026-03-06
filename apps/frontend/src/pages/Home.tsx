02d02d2692ffe5b6b0acc409f11634bea42d84e9
Hairum Qureshi <hqureshi@udel.edu>
2026-03-06 15:51:27 -0500

    styled landing page

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,22 +1,32 @@
-import useGoogleAuth from "../hooks/useGoogleAuth";
-import { useCurrentUser } from "../hooks/useCurrentUser";
+import activityGif from "../assets/live-status.gif";
 
 export default function Home() {
-	const { googleSignInMutation } = useGoogleAuth();
-	const { data: currUserData } = useCurrentUser();
-
 	return (
-		<div className="min-h-screen bg-slate-950 text-white relative overflow-hidden flex flex-col">
-				<div className="flex flex-col items-center gap-4">
-					{!currUserData && (
-						<button
-							className="group relative flex items-center gap-3 bg-white text-slate-900 px-8 py-3.5 rounded-lg font-medium hover:bg-slate-100 transition-all duration-300 shadow-[0_0_20px_rgba(56,189,248,0.3)] hover:shadow-[0_0_30px_rgba(56,189,248,0.5)] hover:cursor-pointer"
-							onClick={() => googleSignInMutation()}
-						>			
-							Continue with Google
-						</button>
-					)}
+		<div className="min-h-screen bg-zinc-900 text-white relative overflow-hidden flex flex-col font-sans">
+			<div className="relative w-full my-30 flex justify-center">
+				{/* Glow */}
+				<div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+					<div className="w-[500px] h-[220px] bg-gradient-to-r from-white/10 via-zinc-200/10 to-white/10 blur-[100px] rounded-full"></div>
 				</div>
+
+				{/* Content */}
+				<div className="relative z-10 text-center">
+					<h1 className="text-7xl">Welcome to Insomnia</h1>
+					<h3 className="text-4xl mt-5 text-zinc-500 font-sans">
+						A hub for late-night writers
+					</h3>
+					<div className="flex items-center justify-center">
+						<img src={activityGif} alt="Activity gif" className="w-12 h-16" />
+						<h3 className="text-xl text-slate-400 font-sans -ml-2">
+							<span className="text-green-400">
+								<strong>10</strong>
+							</span>{" "}
+							night owls are currently writing
+						</h3>
+					</div>
+				</div>
+			</div>
+			<div className="border border-slate-600 p-3 rounded-tl-2xl rounded-tr-2xl bg-zinc-950 w-5/6 mt-10 m-auto min-h-screen max-h-auto"></div>
 		</div>
 	);
 }
