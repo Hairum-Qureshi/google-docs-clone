dbe5845761adbfd7e2d60196d6f2a03e476c226b
Hairum Qureshi <hqureshi@udel.edu>
2026-03-09 09:25:33 -0400

    replaced hardcoded PORT number with ENV PORT variable

--- apps/yjs-backend/collab.ts
+++ apps/yjs-backend/collab.ts
@@ -1,11 +1,15 @@
 import { Hocuspocus } from "@hocuspocus/server";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const PORT = process.env.HOCUSPOCUS_PORT || 8000;
 const server = new Hocuspocus({
-	port: 8000,
+	port: +PORT,
 	async onConnect() {
 		console.log("Client connected! 🔮");
 	}
 });
 
 server.listen();
-console.log("Hocuspocus server running at ws://127.0.0.1:8000");
+console.log(`Hocuspocus server running at ws://127.0.0.1:${PORT}`);
