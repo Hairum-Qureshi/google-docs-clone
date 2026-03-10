588f4ec54b73a8d8c002d42fb2940697413875d6
Hairum Qureshi <hqureshi@udel.edu>
2026-03-09 23:38:55 -0400

    implemented collaboration and user caret visibility for all users that are in the room

--- apps/frontend/src/pages/Document.tsx
+++ apps/frontend/src/pages/Document.tsx
@@ -1,19 +1,67 @@
 import TipTapEditor from "../components/TipTapEditor";
 import Toolbar from "../components/Toolbar";
+import { useEditor } from "@tiptap/react";
+import * as Y from "yjs";
+import { HocuspocusProvider } from "@hocuspocus/provider";
+import Collaboration from "@tiptap/extension-collaboration";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
-import { useEditor } from "@tiptap/react";
+import CollaborationCaret from "@tiptap/extension-collaboration-caret";
+import "../css/index.css";
+import { useCurrentUser } from "../hooks/useCurrentUser";
+import { useMemo } from "react";
+
+// ! if you're typing on the first line, the caret seems to get cut off from the top 
 
 export default function Document() {
-	const editor = useEditor({
-		extensions: [StarterKit, Link, Underline],
-		editorProps: {
-			attributes: {
-				class: "focus:outline-none flex-1 h-full"
-			}
-		}
-	});
+	const { data: currUser } = useCurrentUser();
+
+	const provider = useMemo(() => {
+		return new HocuspocusProvider({
+			url: import.meta.env.VITE_HOCUSPOCUS_WEBSOCKET, // your Hocuspocus WS endpoint
+			name: "document-1",
+			document: new Y.Doc()
+		});
+	}, []); // Empty dependency array means create once
+
+	// 3. Wrap extensions in useMemo so they don't trigger re-renders
+	const extensions = useMemo(
+		() => [
+			StarterKit,
+			Link,
+			Underline,
+			Collaboration.configure({ document: provider.document }),
+			CollaborationCaret.configure({
+				provider,
+				user: {
+					name: currUser
+						? `${currUser.firstName} ${currUser.lastName}`
+						: "Anonymous",
+					color: "#f783ac"
+				},
+				render: user => {
+					const caret = document.createElement("span");
+					caret.classList.add("collaboration-carets__caret");
+					caret.setAttribute(
+						"style",
+						`border-color: ${user.color}; --caret-color: ${user.color}`
+					);
+
+					const label = document.createElement("div");
+					label.classList.add("collaboration-carets__label");
+					label.setAttribute("style", `background-color: ${user.color}`);
+					label.innerText = user.name;
+
+					caret.appendChild(label);
+					return caret;
+				}
+			})
+		],
+		[provider]
+	); // Only recreate if provider changes
+
+	const editor = useEditor({ extensions }); // No need for [provider] here now
 
 	if (!editor) return null;
 
@@ -23,8 +71,8 @@ export default function Document() {
 				<div className="m-auto p-3 flex w-2/3 bg-white tr-rounded-md tl-rounded-md">
 					<Toolbar editor={editor} />
 				</div>
-				<div className="w-2/3 bg-white p-2 h-screen wrap-break-word m-auto">
-					<div className="mx-10">
+				<div className="w-2/3 outline-0 border-0 bg-white p-2 h-[80vh] overflow-hidden m-auto">
+					<div className="mx-10 h-full">
 						<TipTapEditor editor={editor} />
 					</div>
 				</div>
