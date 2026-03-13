7736c1d2ac3f831f0304095bee4812bc04b0433d
Hairum Qureshi <hqureshi@udel.edu>
2026-03-13 11:17:04 -0400

    modified yjs-backend so it autosaves data added to document while collaborating

--- apps/yjs-backend/collab.ts
+++ apps/yjs-backend/collab.ts
@@ -1,17 +1,57 @@
 import { Hocuspocus } from "@hocuspocus/server";
+import axios from "axios";
+import { Database } from "@hocuspocus/extension-database";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.HOCUSPOCUS_PORT || 8000;
 const server = new Hocuspocus({
-	port: +PORT,
-	async onConnect() {
-		console.log("Client connected! 🔮");
-	}
+	port: +process.env.HOCUSPOCUS_PORT! || 8000,
+	async onConnect(data) {
+		// Access the unique connection ID
+		console.log("Client connected");
+	},
+	async onDisconnect(data) {
+		// Access the unique connection ID
+		console.log("Client disconnected");
+	},
+	extensions: [
+		new Database({
+			fetch: async ({ documentName }: { documentName: string }) => {
+				try {
+					console.log(`Attempting to fetch doc: ${documentName}`);
+					const response = await axios.get(
+						`${process.env.NEST_BACKEND}/api/document/${documentName}/binary`,
+						{ responseType: "arraybuffer" }
+					);
+
+					if (response.data && response.data.byteLength > 0) {
+						return new Uint8Array(response.data);
+					}
+
+					// IMPORTANT: Return null for brand-new documents
+					return null;
+				} catch (e) {
+					return null; // Start fresh if 404 or error
+				}
+			},
+			store: async ({
+				documentName,
+				state
+			}: {
+				documentName: string; // this is the document ID that's called when your initialize 'name' in the HocuspocusProvider on the frontend
+				state: Uint8Array;
+			}) => {
+				await axios.patch(
+					`${process.env.NEST_BACKEND}/api/document/${documentName}/update-content`,
+					{
+						state: Buffer.from(state).toString("base64") // Send as base64 over HTTP
+					}
+				);
+			}
+		})
+	]
 });
 
 server.listen();
-console.log(
-	`Hocuspocus server and websocket server running successfully on port ${PORT}!`
-);
