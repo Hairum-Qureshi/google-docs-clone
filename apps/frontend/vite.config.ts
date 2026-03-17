68e038eafda4343696203f745ddf992cfa813d1b
Hairum Qureshi <hqureshi@udel.edu>
2026-03-17 14:45:43 -0400

    resolved whatever issue was going on with Vite/TipTap

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		fs: {
+			allow: ["../.."]
+		}
+	},
 	resolve: {
-		preserveSymlinks: true
+		alias: {
+			"@tiptap/react": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/react"
+			),
+			"@tiptap/starter-kit": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/starter-kit"
+			),
+			"@tiptap/extension-collaboration": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/extension-collaboration"
+			),
+			"@tiptap/extension-collaboration-caret": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/extension-collaboration-caret"
+			),
+			"@tiptap/extension-link": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/extension-link"
+			),
+			"@tiptap/extension-underline": path.resolve(
+				__dirname,
+				"../../node_modules/@tiptap/extension-underline"
+			)
+		}
 	}
 });
