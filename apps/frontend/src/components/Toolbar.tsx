76d7b027b0280dd66ed6a297fa1a53eb83634129
Hairum Qureshi <hqureshi@udel.edu>
2026-03-18 01:29:27 -0400

    added 'text alignment' buttons

--- apps/frontend/src/components/Toolbar.tsx
+++ apps/frontend/src/components/Toolbar.tsx
@@ -6,19 +6,22 @@ import { FaStrikethrough } from "react-icons/fa";
 import { FaQuoteRight } from "react-icons/fa6";
 import { MdOutlineFormatListBulleted } from "react-icons/md";
 import { MdOutlineFormatListNumbered } from "react-icons/md";
+import { GrTextAlignRight } from "react-icons/gr";
+import { GrTextAlignLeft } from "react-icons/gr";
+import { GrTextAlignCenter } from "react-icons/gr";
 
 export default function Toolbar({ editor }: { editor: Editor | null }) {
 	if (!editor) return null;
 
 	return (
 		<div>
-			<div className="flex items-center gap-4 text-black mx-10">
+			<div className="flex items-center gap-4 text-white mx-10">
 				<button
 					onClick={() => editor.chain().focus().toggleBold().run()}
 					disabled={!editor.can().chain().focus().toggleBold().run()}
 				>
 					<HiMiniBold
-						className={`text-lg cursor-pointer ${
+						className={`text-xl cursor-pointer ${
 							editor.isActive("bold") ? "text-blue-500" : "hover:text-zinc-400"
 						}`}
 					/>
@@ -29,7 +32,7 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					disabled={!editor.can().chain().focus().toggleItalic().run()}
 				>
 					<FaItalic
-						className={`text-lg cursor-pointer ${
+						className={`text-xl cursor-pointer ${
 							editor.isActive("italic")
 								? "text-blue-500"
 								: "hover:text-zinc-400"
@@ -42,7 +45,7 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					disabled={!editor.can().chain().focus().toggleUnderline().run()}
 				>
 					<ImUnderline
-						className={`text-lg cursor-pointer ${
+						className={`text-xl cursor-pointer ${
 							editor.isActive("underline")
 								? "text-blue-500"
 								: "hover:text-zinc-400"
@@ -55,7 +58,7 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					disabled={!editor.can().chain().focus().toggleStrike().run()}
 				>
 					<FaStrikethrough
-						className={`text-lg cursor-pointer ${
+						className={`text-xl cursor-pointer ${
 							editor.isActive("strike")
 								? "text-blue-500"
 								: "hover:text-zinc-400"
@@ -68,7 +71,7 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					disabled={!editor.can().chain().focus().toggleBlockquote().run()}
 				>
 					<FaQuoteRight
-						className={`text-lg cursor-pointer ${
+						className={`text-xl cursor-pointer ${
 							editor.isActive("blockquote")
 								? "text-blue-500"
 								: "hover:text-zinc-400"
@@ -76,14 +79,14 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					/>
 				</button>
 
-				<div className="border border-zinc-400 h-5" />
+				<div className="border border-zinc-400 h-8" />
 
 				<button
 					onClick={() => editor.chain().focus().toggleBulletList().run()}
 					disabled={!editor.can().chain().focus().toggleBulletList().run()}
 				>
 					<MdOutlineFormatListBulleted
-						className={`text-xl cursor-pointer ${
+						className={`text-2xl cursor-pointer ${
 							editor.isActive("bulletList")
 								? "text-blue-500"
 								: "hover:text-zinc-400"
@@ -96,8 +99,49 @@ export default function Toolbar({ editor }: { editor: Editor | null }) {
 					disabled={!editor.can().chain().focus().toggleOrderedList().run()}
 				>
 					<MdOutlineFormatListNumbered
-						className={`text-xl cursor-pointer ${
+						className={`text-2xl cursor-pointer ${
 							editor.isActive("orderedList")
+								? "text-blue-500"
+								: "hover:text-zinc-400"
+						}`}
+					/>
+				</button>
+
+				<div className="border border-zinc-400 h-8" />
+
+				<button
+					onClick={() => editor.chain().focus().setTextAlign("left").run()}
+					disabled={!editor.can().chain().focus().setTextAlign("left").run()}
+				>
+					<GrTextAlignLeft
+						className={`text-xl cursor-pointer ${
+							editor.isActive({ textAlign: "left" })
+								? "text-blue-500"
+								: "hover:text-zinc-400"
+						}`}
+					/>
+				</button>
+
+				<button
+					onClick={() => editor.chain().focus().setTextAlign("center").run()}
+					disabled={!editor.can().chain().focus().setTextAlign("center").run()}
+				>
+					<GrTextAlignCenter
+						className={`text-xl cursor-pointer ${
+							editor.isActive({ textAlign: "center" })
+								? "text-blue-500"
+								: "hover:text-zinc-400"
+						}`}
+					/>
+				</button>
+
+				<button
+					onClick={() => editor.chain().focus().setTextAlign("right").run()}
+					disabled={!editor.can().chain().focus().setTextAlign("right").run()}
+				>
+					<GrTextAlignRight
+						className={`text-xl cursor-pointer ${
+							editor.isActive({ textAlign: "right" })
 								? "text-blue-500"
 								: "hover:text-zinc-400"
 						}`}
