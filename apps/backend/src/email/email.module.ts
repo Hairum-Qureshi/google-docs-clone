30c8c1904bc89030bcbc85e5f0291e97b31062bc
Hairum Qureshi <hqureshi@udel.edu>
2026-03-19 00:30:14 -0400

    registered Resend provider

---
+++ apps/backend/src/email/email.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { ResendService } from './resend.service';
+
+@Module({
+  providers: [EmailService, ResendService],
+  controllers: [EmailController],
+  imports: [AuthModule],
+})
+export class EmailModule {}
