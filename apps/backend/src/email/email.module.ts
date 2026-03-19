7cee87745f77cf3f0999b3c72a9d5e103b26870e
Hairum Qureshi <hqureshi@udel.edu>
2026-03-19 00:39:55 -0400

    added 'ConfigModule' to imports array

--- apps/backend/src/email/email.module.ts
+++ apps/backend/src/email/email.module.ts
@@ -3,10 +3,11 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { ResendService } from './resend.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [EmailService, ResendService],
   controllers: [EmailController],
-  imports: [AuthModule],
+  imports: [AuthModule, ConfigModule],
 })
 export class EmailModule {}
