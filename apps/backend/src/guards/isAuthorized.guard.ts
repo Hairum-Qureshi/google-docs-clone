982f2f0dcf8e10f71fa7723fe1952a8b48a1f00e
Hairum Qureshi <hqureshi@udel.edu>
2026-03-19 10:19:13 -0400

    refined conditional logic and utilized Role decorator data to check if role matches user role

--- apps/backend/src/guards/isAuthorized.guard.ts
+++ apps/backend/src/guards/isAuthorized.guard.ts
@@ -10,6 +10,7 @@ import { Model } from 'mongoose';
 import { Role } from 'src/enums/roles.enum';
 import { Doc, DocDocument } from 'src/schemas/Document';
 import { DocumentPermission } from 'src/schemas/DocumentPermission';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class HasRolePermissions implements CanActivate {
@@ -17,11 +18,16 @@ export class HasRolePermissions implements CanActivate {
     @InjectModel(Doc.name) private docModel: Model<DocDocument>,
     @InjectModel(DocumentPermission.name)
     private documentPermissionModel: Model<DocumentPermission>,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const docID: string = context.switchToHttp().getRequest().params.docID;
     const userID: string = context.switchToHttp().getRequest().user._id;
+    const roles: Role[] = this.reflector.get<Role[]>(
+      'roles',
+      context.getHandler(),
+    );
 
     const document = await this.docModel.findById(docID);
     const userPermission = await this.documentPermissionModel.findOne({
@@ -31,12 +37,19 @@ export class HasRolePermissions implements CanActivate {
 
     // first we need to check whether the doc exists
     if (!document) throw new NotFoundException('Document not found');
+    if (!userPermission || document.authorUID !== userID)
+      throw new ForbiddenException('You do not have access to this document');
 
     // next we need to check whether if the user is a document author or editor; if so, grant them access to the document; otherwise, throw a forbidden exception
-    if (document.authorUID === userID || userPermission?.role === Role.EDITOR) {
+    if (
+      document.authorUID === userID ||
+      roles.includes(userPermission.role as Role)
+    ) {
       return true;
     }
 
-    throw new ForbiddenException('You do not have access to this document');
+    throw new ForbiddenException(
+      'You do not have permission to perform this action',
+    );
   }
 }
