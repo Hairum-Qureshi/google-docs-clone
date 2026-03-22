5d410aee0eb102c9c27416435132e68ad9244692
Hairum Qureshi <hqureshi@udel.edu>
2026-03-21 23:29:44 -0400

    created helper component for a User Document

---
+++ apps/frontend/src/components/UserDoc.tsx
@@ -0,0 +1,8 @@
+export default function UserDoc() {
+	return (
+		<div className="bg-zinc-800 p-4 rounded-md h-50 w-50">
+			<h3 className="text-xl font-semibold">Document Title 3</h3>
+			<p className="text-sm text-gray-400">Last edited: 3 days ago</p>
+		</div>
+	);
+}
