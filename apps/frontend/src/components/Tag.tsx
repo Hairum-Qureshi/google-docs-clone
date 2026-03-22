4327703138b7c2d46f7c1740d598b0580bf9669b
Hairum Qureshi <hqureshi@udel.edu>
2026-03-22 00:41:29 -0400

    replaced hardcoded user with dynamic email that's passed in as a prop

--- apps/frontend/src/components/Tag.tsx
+++ apps/frontend/src/components/Tag.tsx
@@ -1,9 +1,9 @@
 import { IoClose } from "react-icons/io5";
 
-export default function Tag() {
+export default function Tag({ email }: { email: string }) {
 	return (
 		<div className="px-2 py-1 hover:cursor-pointer rounded-md bg-slate-700 flex items-center w-max">
-			<p>john@gmail.com</p>
+			<p>{email}</p>
 			<button
 				className="text-red-500 hover:cursor-pointer text-lg hover:text-red-400 ml-3"
 				title="Remove"
