3825a19dfaaea0955051260de467bcd37dcf7ea1
Hairum Qureshi <hqureshi@udel.edu>
2026-03-21 23:45:43 -0400

    removed ConfigModule import

--- apps/backend/src/email/email.module.ts
+++ apps/backend/src/email/email.module.ts
@@ -3,11 +3,10 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { ResendService } from './resend.service';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [EmailService, ResendService],
   controllers: [EmailController],
-  imports: [AuthModule, ConfigModule],
+  imports: [AuthModule],
 })
 export class EmailModule {}
